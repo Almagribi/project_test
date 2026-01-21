826da7ccb8bafadc7521c5fd49c3a947e929db68
Almagribi <jadidalmagribi@gmail.com>
2026-01-21 09:43:54 +0700

    Added darkmode

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Aplikasi berjalan");
+const btn = document.getElementById("btn-action");
+
+btn.addEventListener("click", function () {
+  document.body.classList.toggle("dark");
+
+  console.log(`Dark mode on!`);
+});
